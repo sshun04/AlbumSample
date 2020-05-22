a2f5281e536b1bbbdc4103ad8d866efba99a25d2
sshun04 <shojishunshun@gmail.com>
2020-05-23 04:28:56 +0900

    add query param

--- app/src/main/java/com/shunsukeshoji/albumapp/data/GithubService.kt
+++ app/src/main/java/com/shunsukeshoji/albumapp/data/GithubService.kt
@@ -1,8 +1,10 @@
 package com.shunsukeshoji.albumapp.data
 
 import retrofit2.http.GET
+import retrofit2.http.Query
+import kotlin.random.Random
 
 interface GithubService {
     @GET("users")
-    suspend fun getUser(): List<User>
+    suspend fun getUser(@Query(value = "since") since:Int = Random.nextInt(1000000)): List<User>
 }
